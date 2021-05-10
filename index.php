bea6d7410b71b0d748f0c31f0d7b51d38cd5722f
Temisan <73684278+heytemisan@users.noreply.github.com>
2021-05-10 11:32:46 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Portfolio</title>
+    <!-- lightbox css -->
+    <link rel="stylesheet" href="css/lightbox.min.css">
+    <!--bootstrap5-->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/bootstrap-utilities.css">
+    <link rel="stylesheet" href="all.css">
+    <!--customized css-->
+    <link rel="stylesheet" href="style.css">
+
+
+</head>
+<body>
+ <nav class="navbar navbar-expand-md navbar-warning navbar-light fixed-top" id="header">
+    <div class="container">
+        <a class="navbar-brand text-light" href="#">Temisan Portfolio <i class="fas fa-portrait"></i></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+            <a class="nav-link active text-light" aria-current="page" href="#">Home <i class="fas fa-home"></i></a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active text-light" href="#about">About me</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active text-light" href="#project">My Project</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active text-light" href="#service">Services</i></a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active text-light" href="#contact">Contact</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</nav>
+<br><br>
+<br class="mb-5">
+<br>
+<div class="container">
+	<div class="row">
+	<svg viewBox="0 0 960 300">
+    <symbol id="s-text">
+		<text text-anchor="middle" x="50%" y="80%">Temisan</text>
+	</symbol>
+
+	<g class = "g-ants">
+		<use xlink:href="#s-text" class="text-copy"></use>
+		<use xlink:href="#s-text" class="text-copy"></use>
+		<use xlink:href="#s-text" class="text-copy"></use>
+		<use xlink:href="#s-text" class="text-copy"></use>
+		<use xlink:href="#s-text" class="text-copy"></use>
+	</g>
+    </svg>
+	</div>
+    
+    
+    </div>
+<br><br><br>
+<br><br>
+<br id="about">
+<div class="container about card mt-5" >
+        <h2 class="text-center text-white my-3 mb-2 me">About Me <i class="fas fa-graduation-cap text-white"></i></h2>
+    <div class="row">
+        <div class="col-md-12 card-body">
+            <div class="row content text-center mb-2 ">
+                <div class="card bg-dark contentBx p-5">
+                  <p class="lead  scrollto">My Name is Momodu Temisan, I am a web developer from Nigeria.</p>
+                  <p class="lead">I specialize in both frontend development and Backend Programming language such as PHP, SQL & MYSQL</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br><br><br>
+<br  id="project"><br><br><hr>
+
+<div class="container project">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 text-center">
+                <div class="section-title text-white">My Projects <i class="fas fa-folder-open"></i></div>
+                <h2 class="lead mb-5 text-white">Web  Dev  Work  I've  Done</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-12">
+                <div class="card border border-dark">
+                <a href="img/crud.png" data-lightbox="example-4" data-title="login page">
+                <img src="img/crud.png" class="img-fluid" alt=""></a>
+                <div class="card-body text-center bg-dark">
+                    <div class="card-title text-white">Product Crud</div>
+                    <p class="card-text mt-3 text-white" style="font-size: 12px;">Product CRUD app using PHP</p>
+                    <a href="https://github.com/temisann/productCrudApp"><i class="fab fa-github fa-3x"></i></a>
+                </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <div class="card border border-dark">
+                <a href="img/hms.png" data-lightbox="example-4" data-title="login page">
+                <img src="img/hms.png" class="img-fluid" alt=""></a>
+                <div class="card-body text-center bg-dark">
+                    <div class="card-title text-white">A Hospital Management System</div>
+                    <p class="card-text mt-3 text-white" style="font-size: 12px;">This is a hospital management app</p>
+                    <a href="https://github.com/temisann/hospital-management-system"><i class="fab fa-github fa-3x"></i></a>
+                </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <div class="card border border-dark">
+                <a href="img/blog.png" data-lightbox="example-4" data-title="login page">
+                <img src="img/blog.png" class="img-fluid" alt=""></a>
+                <div class="card-body text-center bg-dark">
+                    <div class="card-title text-white">A Personal Blog</div>
+                    <p class="card-text mt-3 text-white" style="font-size: 12px;">A Personal blog using bootstap</p>
+                    <a href="https://github.com/temisann/hospital-management-system"><i class="fab fa-github fa-3x"></i></a>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<br id="service">
+<br>
+<hr>
+<br>
+<br>
+<section class="section container">
+    <div class="container">
+        <div class="row">
+           <div class="col-md-12 text-center">
+              <h2 class="section-title mb-2 text-white">My Services </h2>
+              <p class="section-subtitle text-white lead">The Best Services Provided by Momodu Temisan</p>
+           </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3 mt-3">
+            <div class="card">
+                <img src="img/54425-html-5.gif" alt="">
+                <div class="card-body text-center bg-dark">
+                    <div class="card-title text-white">Programming</div>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3  mt-3">
+            <div class="card">
+                <img src="img/29258-webdesign.gif" alt="">
+                <div class="card-body text-center bg-dark">
+                    <div class="card-title text-white">Web Design</div>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 mt-3">
+            <div class="card">
+               <img src="img/27857-blogcover.gif" alt="">
+                <div class="card-body text-center bg-dark">
+                    <div class="card-title text-white">Blogging</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 mt-3">
+            <div class="card">
+                <img src="img/47289-digital-marketing.gif" alt="">
+                <div class="card-body text-center bg-dark">
+                    <div class="card-title text-white">Digital Marketing</div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<br id="contact">
+<br>
+<br>
+<div class="container project card mt-5 border-dark">
+        <h2 class="text-center my-4 mb-2 text-white">Contact <i class="fa fa-phone-square" aria-hidden="true"></i></h2>
+    <div class="row">
+        <div class="col-md-12 card-body">
+            <div class="row content text-center">
+                <div class="card contact contentBx p-2 border-dark">
+                  <p class="lead">If you have any questions or want to get in touch, feel free to contact me below using these platforms</p>
+                 <ul class="p-1 container font-weight-bold social-link">
+                  <a href="https://twitter.com/heytemisan" class="p-2"><i class="fab fa-twitter-square lead"></i></i></a>
+                  <a href="https://facebook.com/temy.momodu" class="p-2"><i class="fab fa-facebook lead"></i></i></a>
+                  <a href="https://instagram.com/heytemisan" class="p-2"><i class="fab fa-instagram lead"></i></i></a>
+                  <p class="text-center text-white mt-2"> Tel: +23409037424966<i class="fas fa-phone-square "></i></p> 
+                 </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<div class="container project card mt-5"  id="form">
+        <h4 class="sent-notification"></h4>
+        <h2 class="text-center text-light my-3 mb-2 ">Get in touch <i class="fas fa-envelope"></i></h2>
+    <div class="row">
+        <div class="col-md-12 card-body">
+            <div class="row content text-center">
+                <div class="card bg-dark contentBx p-5">
+                  <p class="lead">If you need to send me any message or information, please use the form located below:</p>
+        <form action ="https://formspree.io/f/xleadlnj" method="POST">            
+        
+            <div class="input-group input-group-sm mb-3">
+                <span class="input-group-text span text-white border border-dark" id="inputGroup-sizing-sm">Name</span>
+                <input type="text" class="form-control" aria-label="Sizing example input" name="name" aria-describedby="inputGroup-sizing-sm"  style="background-color: rgb(17, 40, 70); color: white; font-family: cursive;" required>
+            </div>
+            <br>
+
+            <div class="input-group input-group-sm mb-3">
+                <span class="input-group-text span text-white border border-dark" id="inputGroup-sizing-sm">Email</span>
+                <input type="text" class="form-control" aria-label="Sizing example input" name="email" aria-describedby="inputGroup-sizing-sm"  style="background-color: rgb(17, 40, 70); color: white; font-family: cursive;" required>
+            </div>
+        
+            <br>
+
+            <div class="input-group input-group-sm mb-3">
+                <span class="input-group-text span text-white border border-dark" id="inputGroup-sizing-sm">Subject</span>
+                <input type="text" class="form-control" aria-label="Sizing example input" name="subject" aria-describedby="inputGroup-sizing-sm" style="background-color: rgb(17, 40, 70); color: white; font-family: cursive;" required>
+            </div>
+
+            <br>
+            <div class="form-group">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text span text-white border border-dark" id="inputGroup-sizing-sm">Message</span>
+                    <textarea rows="5"  class="form-control"  name="message" required style="background-color: rgb(17, 40, 70); color: white; font-family: cursive;"></textarea>
+                </div>
+               
+            </div>
+            <br>
+            <div class="text-center">
+                <input type="submit" class="btn btn-block w-75 border-dark" style="color: white;" name="submit">Submit</input>
+            </div>
+        </form>
+        </div>
+    </div>
+</div>
+    
+        
+<br>
+<footer id="main-footer" class="p-5 resume text-dark">
+    <div class="row">
+        <div class="col-md-6">
+            <a href="" class="btn btn-dark lead text-white"><i class="fa fa-cloud lead"></i> Download Resume</a>
+        </div>
+        <!-- <div class="col-md-6 copyright">
+            <div class="text-right text-light lead">Copyright <i class="fas fa-copyright"></i> 2021</div>
+        </div> -->
+        </div>
+    </div>
+  </footer>
+  <br>
+  <div class="text-center scroll">
+    <a href="#" class="btn btn-outline-white my-2"><i class="fa fa-arrow-up lead text-white"></i></a>
+  </div>
+
+  <div id="preloader"></div>
+
+<script src="js/all.js"></script>
+<script src="js/bootstrap.bundle.js"></script>
+<script src="js/lightbox-plus-jquery.min.js"></script>
+<script src="js/jquery/jquery.min.js"></script>
+<script src="js/jquery.easing/jquery.easing.min.js"></script>
+<script src="js/sweetalert2@10.js"></script>
+<script type="text/javascript">
+    //prevent resubmission on page refresh
+
+    if (window.history.replaceState) {
+        window.history.replaceState(null, null, window.location.href); 
+    }
+</script>
+
+</body>
+</html>
